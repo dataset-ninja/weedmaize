638fbed6bbf024c95927d8c8edc3eb09c776f214
vorozhkog <vorozhkog@icloud.com>
2023-06-11 13:44:55 +0100

    stats done

--- src/convert.py
+++ src/convert.py
@@ -1,15 +1,106 @@
 # Path to the original dataset
 
 import supervisely as sly
+import os
+import xml.etree.ElementTree as ET
+import xmltodict
 
 
 def convert_and_upload_supervisely_project(
     api: sly.Api, workspace_id: int, project_name: str
 ) -> sly.ProjectInfo:
-    # * dataset_path = "Path to the local dir with dataset"
+    # create project
+    project = api.project.get_info_by_name(workspace_id, project_name)
+    if project is not None:
+        api.project.remove(project.id)
+    project = api.project.create(workspace_id, project_name)
+    meta = sly.ProjectMeta()
 
-    # Function should read local dataset and upload it to Supervisely project, then return project info.
+    datasets = [
+        r"C:\Users\German\Documents\weedmaize\test_set",
+        r"C:\Users\German\Documents\weedmaize\train_set",
+        r"C:\Users\German\Documents\weedmaize\validation_set",
+    ]
 
-    raise NotImplementedError("The converter should be implemented manually.")
+    # cycle through datasets
+    for single_dataset in datasets:
+        mask_path = sly.fs.list_files(
+            single_dataset,
+            valid_extensions=[
+                ".xml",
+            ],
+        )
+        dataset = api.dataset.create(project.id, os.path.basename(single_dataset))
+        for path in mask_path:
+            tree = ET.parse(path)
+            xml_data = tree.getroot()
+            xmlstr = ET.tostring(xml_data, encoding="utf-8", method="xml")
 
-    # * return project
+            data_dict = dict(xmltodict.parse(xmlstr))
+            # get image path and upload
+            image_path = os.path.join(
+                single_dataset,
+                data_dict["annotation"]["filename"],
+            )
+            image_info = api.image.upload_path(
+                dataset.id, data_dict["annotation"]["filename"], image_path
+            )
+
+            labels = []
+
+            if type(data_dict["annotation"]["object"]) == list:
+                for obj in data_dict["annotation"]["object"]:
+                    # get bbox cords
+                    xmin = int(obj["bndbox"]["xmin"])
+                    ymin = int(obj["bndbox"]["ymin"])
+                    xmax = int(obj["bndbox"]["xmax"])
+                    ymax = int(obj["bndbox"]["ymax"])
+
+                    # get class name, create labels
+                    bbox = sly.Rectangle(top=ymin, left=xmin, bottom=ymax, right=xmax)
+                    class_name = obj["name"]
+                    obj_class = meta.get_obj_class(class_name)
+                    if obj_class is None:
+                        obj_class = sly.ObjClass(class_name, sly.Rectangle)
+                        meta = meta.add_obj_class(obj_class)
+                        api.project.update_meta(project.id, meta)
+                    label = sly.Label(bbox, obj_class)
+                    labels.append(label)
+
+                # upload annotation
+                ann = sly.Annotation(
+                    img_size=[
+                        int(data_dict["annotation"]["size"]["height"]),
+                        int(data_dict["annotation"]["size"]["width"]),
+                    ],
+                    labels=labels,
+                )
+                api.annotation.upload_ann(image_info.id, ann)
+                print(f"uploaded bbox to image(id:{image_info.id})")
+            else:
+                xmin = int(data_dict["annotation"]["object"]["bndbox"]["xmin"])
+                ymin = int(data_dict["annotation"]["object"]["bndbox"]["ymin"])
+                xmax = int(data_dict["annotation"]["object"]["bndbox"]["xmax"])
+                ymax = int(data_dict["annotation"]["object"]["bndbox"]["ymax"])
+                bbox = sly.Rectangle(top=ymin, left=xmin, bottom=ymax, right=xmax)
+                class_name = data_dict["annotation"]["object"]["name"]
+                obj_class = meta.get_obj_class(class_name)
+                if obj_class is None:
+                    obj_class = sly.ObjClass(class_name, sly.Rectangle)
+                    meta = meta.add_obj_class(obj_class)
+                    api.project.update_meta(project.id, meta)
+                label = sly.Label(bbox, obj_class)
+                labels.append(label)
+
+                # upload annotation
+                ann = sly.Annotation(
+                    img_size=[
+                        int(data_dict["annotation"]["size"]["height"]),
+                        int(data_dict["annotation"]["size"]["width"]),
+                    ],
+                    labels=labels,
+                )
+                api.annotation.upload_ann(image_info.id, ann)
+                print(f"uploaded bbox to image(id:{image_info.id})")
+        print(f"Dataset {dataset.id} has been successfully created.")
+    return project
--- src/settings.py
+++ src/settings.py
@@ -1,79 +1,97 @@
-from typing import Dict, List, Optional, Union
-
-from dataset_tools.templates import AnnotationType, CVTask, Industry, License
+from typing import Dict, List, Optional
 
 ##################################
 # * Before uploading to instance #
 ##################################
-PROJECT_NAME: str = None
-PROJECT_NAME_FULL: str = None
+PROJECT_NAME: str = "WeedMaize"
+PROJECT_NAME_FULL: str = "WeedMaize"
 
 ##################################
 # * After uploading to instance ##
 ##################################
-LICENSE: License = None
-INDUSTRIES: List[Industry] = None
-CV_TASKS: List[CVTask] = None
-ANNOTATION_TYPES: List[AnnotationType] = None
+LICENSE: str = "CC BY-SA 4.0"
+# Available licenses: ["CC0", "CC BY-SA 4.0"]
 
-RELEASE_YEAR: int = None
-HOMEPAGE_URL: str = None
+INDUSTRIES: List[str] = ["general domain"]
+# Available industries: ["general domain"]
+
+CV_TASKS: List[str] = ["instance segmentation"]
+# Available cv tasks: ["semantic segmentation", "instance segmentation"]
+
+ANNOTATION_TYPES: List[str] = ["instance segmentation"]
+# Available annotation types: ["semantic segmentation", "instance segmentation"]
+
+RELEASE_YEAR: int = 2021
+HOMEPAGE_URL: str = "https://zenodo.org/record/5106795#.Yk_sVn9Bzmg"
 # e.g. "https://some.com/dataset/homepage"
 
-PREVIEW_IMAGE_ID: int = None
+PREVIEW_IMAGE_ID: int = 350842
 # This should be filled AFTER uploading images to instance, just ID of any image.
 
-GITHUB_URL: str = None
+GITHUB_URL: str = "https://github.com/dataset-ninja/weedmaize"
 # URL to GitHub repo on dataset ninja (e.g. "https://github.com/dataset-ninja/some-dataset")
 
 ##################################
 ### * Optional after uploading ###
 ##################################
-DOWNLOAD_ORIGINAL_URL: Optional[Union[str, dict]] = None
+DOWNLOAD_ORIGINAL_URL: Optional[str] = None
 # Optional link for downloading original dataset (e.g. "https://some.com/dataset/download")
 
 CLASS2COLOR: Optional[Dict[str, List[str]]] = None
 # If specific colors for classes are needed, fill this dict (e.g. {"class1": [255, 0, 0], "class2": [0, 255, 0]})
 
-PAPER: Optional[str] = None
-CITATION_URL: Optional[str] = None
-ORGANIZATION_NAME: Optional[Union[str, List[str]]] = None
-ORGANIZATION_URL: Optional[Union[str, List[str]]] = None
-TAGS: List[str] = None
+##################################
+#### ? Templates. Do not edit ####
+##################################
+
+LICENSE_URLS = {
+    "CCO": "https://creativecommons.org/publicdomain/zero/1.0/",
+    "CC BY-SA 4.0": "https://creativecommons.org/licenses/by-sa/4.0/",
+    "MIT License": "https://spdx.org/licenses/MIT.html",
+}
+
+LICENSE_TEXTS = {
+    "CCO": "CC0 1.0 Universal (CC0 1.0) Public Domain Dedication",
+    "CC BY-SA 4.0": "Creative Commons Attribution-ShareAlike 4.0 International (CC BY-SA 4.0)",
+    "MIT License": "MIT License",
+}
+
+CITATION_TEMPLATE = (
+    "If you make use of the {project_name_full} data, "
+    "please cite the following reference (to be prepared after the challenge workshop) "
+    "in any publications:\n\n"
+    "```\n@misc{{{project_name},\n"
+    '\tauthor = "Benedikt Geisler",\n'
+    '\ttitle = "{project_name_full}",\n'
+    '\thowpublished = "{homepage_url}"\n}}\n```\n\n'
+    "[🔗 Source]({homepage_url})"
+)
+
+LICENSE_TEMPLATE = "{project_name_full} data uses [{license_text}]({license_url})."
+
+README_TEMPLATE = "# {project_name_full}\n\n{project_name} is a dataset for {cv_tasks} tasks."
 
 ##################################
 ###### ? Checks. Do not edit #####
 ##################################
 
 
-def check_names():
+def check_before_upload():
     fields_before_upload = [PROJECT_NAME, PROJECT_NAME_FULL]
     if any([field is None for field in fields_before_upload]):
         raise ValueError("Please fill all fields in settings.py before uploading to instance.")
 
 
-def get_settings():
-    settings = {
-        "project_name": PROJECT_NAME,
-        "project_name_full": PROJECT_NAME_FULL,
-        "license": LICENSE,
-        "industries": INDUSTRIES,
-        "cv_tasks": CV_TASKS,
-        "annotation_types": ANNOTATION_TYPES,
-        "release_year": RELEASE_YEAR,
-        "homepage_url": HOMEPAGE_URL,
-        "preview_image_id": PREVIEW_IMAGE_ID,
-        "github_url": GITHUB_URL,
-    }
-    if any([field is None for field in settings.values()]):
+def check_after_upload():
+    fields_after_upload = [
+        LICENSE,
+        INDUSTRIES,
+        CV_TASKS,
+        ANNOTATION_TYPES,
+        RELEASE_YEAR,
+        HOMEPAGE_URL,
+        PREVIEW_IMAGE_ID,
+        GITHUB_URL,
+    ]
+    if any([field is None for field in fields_after_upload]):
         raise ValueError("Please fill all fields in settings.py after uploading to instance.")
-
-    settings["download_original_url"] = DOWNLOAD_ORIGINAL_URL
-    settings["class2color"] = CLASS2COLOR
-    settings["paper"] = PAPER
-    settings["citation_url"] = CITATION_URL
-    settings["organization_name"] = ORGANIZATION_NAME
-    settings["organization_url"] = ORGANIZATION_URL
-    settings["tags"] = TAGS if TAGS is not None else []
-
-    return settings
